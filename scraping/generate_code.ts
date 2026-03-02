786fd933977dd6e24564cf193ecbef99c3273013
pluswing <toshiyuki@pluswing.co.jp>
2026-03-02 09:58:06 +0900

    #033 コード生成処理ほぼ完成？

--- scraping/generate_code.ts
+++ scraping/generate_code.ts
@@ -5,60 +5,94 @@ import {
 import assert from "node:assert";
 
 const ADDRESSING_MODE_TABLE: {[key: string]: string} = {
-  "Program Counter Relative": "",
+  "Program Counter Relative": "Program_Counter_Relative",
   "Implied (type 1)": "Implied",
   "Absolute": "Absolute",
-  "Stack (Push)": "",
-  "Stack (PC Relative Long)": "",
-  "Stack (Pull)": "",
-  "Stack (RTI)": "",
-  "Implied (type 2)": "",
-  "Immediate": "",
-  "Implied (type 3)": "",
+  "Direct Page": "Direct_Page",
+  "Direct Page Indexed by X": "Direct_Page_Indexed_by_X",
+  "Stack (Push)": "Stack_Push",
+  "Stack (PC Relative Long)": "Stack_PC_Relative_Long",
+  "Stack (Pull)": "Stack_Pull",
+  "Stack (RTI)": "Stack_RTI",
+  "Implied (type 2)": "Implied",
+  "Immediate": "Immediate",
+  "Absolute Long": "Absolute_Long",
+  "Direct Page Indirect": "Direct_Page_Indirect",
+  "Direct Page Indirect Long": "Direct_Page_Indirect_Long",
+  "Absolute Indexed by X": "Absolute_Indexed_by_X",
+  "Absolute Long Indexed by X": "Absolute_Long_Indexed_by_X",
+  "Absolute Indexed by Y": "Absolute_Indexed_by_Y",
+  "Direct Page Indexed Indirect by X": "Direct_Page_Indexed_Indirect_by_X",
+  "Direct Page Indirect Indexed by Y": "Direct_Page_Indirect_Indexed_by_Y",
+  "Direct Page Indirect Long Indexed by Y": "Direct_Page_Indirect_Long_Indexed_by_Y",
+  "Stack Relative": "Stack_Relative",
+  "Stack Relative Indirect Indexed by Y": "Stack_Relative_Indirect_Indexed_by_Y",
+  "Direct Page Indexed by Y": "Direct_Page_Indexed_by_Y",
+  "Absolute Indirect": "Absolute_Indirect",
+  "Absolute Indexed Indirect": "Absolute_Indexed_Indirect",
+  "Absolute Indirect Long": "Absolute_Indirect_Long",
+  "Implied (type 3)": "Implied",
   "Accumulator": "Accumulator",
-  "Stack (absolute)": "",
-  "Stack (RTL)": "",
-  "Block Move": "",
-  "Stack (Interrupt)": "",
+  "Stack (absolute)": "Stack_absolute",
+  "Stack (RTL)": "Stack_RTL",
+  "Block Move": "Block_Move",
+  "Stack (Interrupt)": "Stack_Interrupt",
   "Program Counter Relative Long": "Program_Counter_Relative_Long",
-  "Implied (type 3)[4]": "",
-  "Stack (RTS)": "",
-  "Stack (Direct Page Indirect)": "",
+  "Implied (type 3)[4]": "Implied",
+  "Stack (RTS)": "Stack_RTS",
+  "Stack (Direct Page Indirect)": "Stack_Direct_Page_Indirect"
 }
 
-const addressingModes: string[] = []
 const main = () => {
+  outputHeader()
+  outputOpsDefinition()
+  outputCallFunction()
+}
+
+const outputHeader = () => {
+  console.log(`
+use std::{collections::HashMap};
+use once_cell::sync::Lazy;
+use crate::cpu::{AddressingMode, CPU, CycleCalcMode, FLAG_MEMORY_ACCUMULATOR_MODE, MODE_16BIT, OpCode};
+`.trim())
+}
+
+const outputOpsDefinition = () => {
+  console.log(`
+pub static CPU_OPS_CODES: Lazy<HashMap<u8, OpCode>> = Lazy::new(|| {
+  let mut m = HashMap::new();
+`.trim())
   const files = fs.readdirSync("ops")
   for (const file of files) {
     const opname = file.split(".")[0]
     const html = fs.readFileSync(`ops/${file}`).toString()
     const doc = new DOMParser().parseFromString(html, "text/html")
     const table = doc.querySelector(".wikitable")
-    const tbody = table?.querySelectorAll("tbody")[2]
-    const trs = tbody?.querySelectorAll("tr")!
-    for (const tr of trs) {
-      const tds = tr.querySelectorAll("td")
-      const addressingMode = tds[0].innerText.trim()
-      const opcode = tds[1].innerText.trim()
-      const bytes = tds[2].innerText.trim()
-      const clocks = tds[3].innerText.trim()
-      console.log({
-        addressingMode,
-        opcode,
-        bytes,
-        clocks
-      })
-      const [emulationBytes, nativeBytes] = parseBytes(bytes)
-      const [emulationClocks, nativeClocks] = parseClocks(clocks)
+    const tbodies = table?.querySelectorAll("tbody")
+    for (let i = 0; i < tbodies!.length; i++) {
+      if (i < 2) {
+        continue;
+      }
+      const tbody = tbodies![i]
+      const trs = tbody?.querySelectorAll("tr")!
+      for (const tr of trs) {
+        const tds = tr.querySelectorAll("td")
+        const addressingMode = tds[0].innerText.trim()
+        const opcode = tds[1].innerText.trim()
+        const bytes = tds[2].innerText.trim()
+        const clocks = tds[3].innerText.trim()
+        const [emulationBytes, nativeBytes] = parseBytes(bytes)
+        const [emulationClocks, nativeClocks] = parseClocks(clocks)
+        const am = ADDRESSING_MODE_TABLE[addressingMode]
+        assert(am)
 
-      console.log(`
-  m.insert(0x${opcode}, OpCode::new(0x${opcode}, "${opname}", OpInfo::new(${nativeBytes}, ${nativeClocks}), OpInfo::new(${emulationBytes}, ${emulationClocks}), AddressingMode::${ADDRESSING_MODE_TABLE[addressingMode]}));
-  `)
-
-      addressingModes.push(addressingMode)
+        console.log(`  m.insert(0x${opcode}, OpCode::new(0x${opcode}, "${opname}", OpInfo::new(${nativeBytes}, ${nativeClocks}), OpInfo::new(${emulationBytes}, ${emulationClocks}), AddressingMode::${am}));`)
+      }
     }
   }
-  console.log([...new Set(addressingModes)])
+  console.log(`
+  m
+});`.trim())
 }
 
 const parseBytes = (rawBytes: string): [number, number] => {
@@ -81,6 +115,30 @@ const parseClocks = (rawCycles: string): [number, number] => {
   assert(rawCycles.match(/^[1-9] cycles\*?$/), rawCycles)
   const nums = rawCycles.replace(" cycles", "").replace("*", "")
   return [parseInt(nums), parseInt(nums)]
+}
+
+const outputCallFunction = () => {
+  console.log(`
+pub fn call(cpu: &mut CPU, op: &OpCode) {
+  match op.name.as_str() {
+    `)
+
+  const files = fs.readdirSync("ops")
+  for (const file of files) {
+    const opname = file.split(".")[0]
+    console.log(`
+  "${opname}" => {
+      cpu.${opname.toLowerCase()}(&op.addressing_mode);
+      if cpu.is_native_move() {
+      } else {
+      }
+      cpu.program_counter = cpu.program_counter.wrapping_add(op.bytes - 1);
+    }
+`)
+  }
+    console.log(`
+  }
+`)
 }
 
 main()
