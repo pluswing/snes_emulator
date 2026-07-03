17055175f6b4ddd3b671617a9d09ae4dd3fb13d1
pluswing <toshiyuki@pluswing.co.jp>
2026-07-03 10:33:53 +0900

    #103 DMA処理実装。バグあり。

--- src/bus.rs
+++ src/bus.rs
@@ -1,3 +1,7 @@
+use core::panic;
+
+use sdl3::libc::PROC_PIDTBSDINFO;
+
 use crate::{cartridge::{self, Cartridge}, ppu::PPU};
 
 pub struct Bus {
@@ -9,6 +13,7 @@ pub struct Bus {
   pub memory: Vec<u8>, // size=0xFFFFFF
 
   // DMA
+  mdmean: u8, // 420Bh WO - MDMAEN  - GDMAチャネルレジスタ
   dmap0: u8, // 43x0h RW - DMAPx   - DMA設定レジスタ
   bbad0: u8, // 43x1h RW - BBADx   - DBバスアドレス
   a1t0l: u8, // 43x2h RW - A1TxL   - Aバスアドレス (low)
@@ -32,6 +37,8 @@ impl Bus {
       ppu,
       cartridge,
       memory: vec![0; 0x100_0000],
+
+      mdmean: 0x00,
       dmap0: 0xFF,
       bbad0: 0xFF,
       a1t0l: 0xFF,
@@ -51,16 +58,48 @@ impl Bus {
     // ~
     // 43xFh RW - MIRRx   - 43xBhのミラー (R/W)
     match addr {
-      0x4300 => {
-        self.dmap0 = data;
+      0x420B => {
+        // DMA有効
+        self.mdmean = data;
         self.do_transfer();
       }
+      0x4300 => {
+        // 転送設定
+        self.dmap0 = data; // 0x09 = 00001001
+      }
+      // PPU アドレス (to)
+      0x4301 => self.bbad0 = data,
+      // Memory アドレス (from)
+      0x4302 => self.a1t0l = data,
+      0x4303 => self.a1t0h = data,
+      0x4304 => self.a1b0 = data,
+      // 転送サイズ
+      0x4305 => self.das0l = data,
+      0x4306 => self.das0h = data,
       _ => panic!("not implemented write_dma_registers({:04X}, {:02X})", addr, data)
     }
   }
 
   fn do_transfer(&mut self) {
-    // TODO
+    if self.mdmean & 0x01 != 0 {
+      // DMAチャネル0が有効 => 転送する！
+
+      // CPUメモリから読み込み、PPUレジスタ
+      // 1バイトごとにDMAアドレスがインクリメント
+      // DMAアドレスは固定されない
+      // 2レジスタ1書き込み	2 バイト: p, p+1
+      let mut memory_addr = (self.a1b0 as u32) << 16 | (self.a1t0h as u32) << 8 | (self.a1t0l as u32);
+      let ppu_addr = 0x002100 | (self.bbad0 as u32); // $00:2100 ～ $00:21ff
+      let transfer_size = (self.das0h as u32) << 8 | (self.das0l as u32);
+      let transfer_size = if transfer_size == 0 { 0x10000 } else { transfer_size };
+
+      // do_transfer M: 00CD35, P: 002118, S: 10000
+      for i in 0..transfer_size {
+        let v = self.mem_read(memory_addr);
+        self.mem_write(if (i % 2) == 0 { ppu_addr } else { ppu_addr + 1 }, v);
+        memory_addr = (memory_addr & 0xFF0000) | ((memory_addr + 1) & 0x00FFFF);
+      }
+    }
   }
 
 }
@@ -131,8 +170,7 @@ impl Mem for Bus {
           0x0000..=0x1FFF => self.wram[addr as usize] = data,
           0x2100..=0x213F => self.ppu.write(addr, data),
           0x420B => {
-            // 420Bh WO - MDMAEN  - GDMAチャネルレジスタ 0
-            // panic!("MDMAEN");
+            self.write_dma_registers(addr, data);
           },
           0x4200..=0x420D => {
             // 4200h WO - NMITIMEN- 割り込み有効化レジスタ
